870677bede44b7ac1d598fc1ba795478708652cd
Kern <kern.walster@gmail.com>
2017-05-15 21:42:03 -0500

    Fix typo in set 2 challenge 9

--- set2/set2.go
+++ set2/set2.go
@@ -6,7 +6,7 @@ import "encoding/hex"
 // RunChallenge9 tests that set2 challenge9 has been correctly implemented
 func RunChallenge9() {
 	util.PrintChallengeHeader(2, 9)
-	input := "YELLOW SUBMARIN"
+	input := "YELLOW SUBMARINE"
 	// "YELLOW SUBMARINE\x04\x04\x04\x04" hex encoded
 	expected := "59454c4c4f57205355424d4152494e4504040404"
 	actual := util.AddPkcs7Padding([]byte(input), 20)
